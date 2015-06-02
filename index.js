169c2eeefa959196fa26e77c28db68952760d4f9
Robin Hawkes <rob@rawkes.com>
2015-06-02 13:06:24 +0100

    Implemented basic normals repair

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var _ = require("lodash");
+var async = require("async");
 var fs = require("fs-extra");
 var path = require("path");
 var sax = require("sax");
@@ -12,6 +13,7 @@ var citygmlSRS = require("citygml-srs");
 var citygmlPolygons = require("citygml-polygons");
 var citygmlBoundaries = require("citygml-boundaries");
 var citygmlPoints = require("citygml-points");
+var citygmlValidateShell = require("citygml-validate-shell");
 
 // Other helpers
 var polygons2obj = require("polygons-to-obj");
@@ -76,47 +78,86 @@ var citygmlToObj = function(citygmlPath, objPath) {
 
     var id = xmlDOM.firstChild.getAttribute("gml:id") || UUID.v4();
 
-    var polygons = citygmlPolygons(xml);
-
+    var polygonsGML = citygmlPolygons(xml);
     var allPolygons = [];
-    var allFaces = [];
 
-    // TODO: Support polygons with holes
-    _.each(polygons, function(polygon) {
+    _.each(polygonsGML, function(polygonGML) {
       // Get exterior and interior boundaries for polygon (outer and holes)
-      var boundaries = citygmlBoundaries(polygon);
+      var boundaries = citygmlBoundaries(polygonGML);
 
       // Get vertex points for the exterior boundary
       var points = citygmlPoints(boundaries.exterior[0]);
 
-      // Triangulate faces
-      var faces = triangulate(points);
-
       allPolygons.push(points);
-      allFaces.push(faces);
     });
 
-    // Create OBJ using polygons and faces
-    var objStr = polygons2obj(allPolygons, allFaces, true);
-    // console.log(objStr);
+    // Process control
+    async.waterfall([function(callback) {
+      // Validate CityGML
+      citygmlValidateShell(polygonsGML, function(err, results) {
+        callback(err, results);
+      });
+    }, function(results, callback) {
+      // Repair CityGML
+      var polygonsCopy = _.clone(allPolygons);
 
-    var outputPath = path.join(objPath, id + ".obj");
+      _.each(results, function(vError) {
+        // Should always be an error, but check anyway
+        if (!vError || !vError[0]) {
+          return;
+        }
 
-    // TODO: Fix huge delay before these small files appear in the filesystem when using fsextra.outputFile
-    // - Possibly a local issue with my filesystem rather than a Node or app issue
-    // - Perhaps batching will help, as well as deferring saving until CityGML buffer processing is complete
-    fs.outputFile(outputPath, objStr, function(err) {
-      if (err) {
-        console.error(err);
-      }
+        // Failure indexes, for repair
+        var vIndices = vError[1];
 
-      // console.log("Saved:", id);
-    });
+        // Output validation error name
+        switch (vError[0].message.split(":")[0]) {
+          case "GE_S_POLYGON_WRONG_ORIENTATION":
+            console.log("Reverse vertices for polygons:", vIndices);
+
+            _.each(vIndices, function(vpIndex) {
+              polygonsCopy[vpIndex].reverse();
+            });
+
+            break;
+        }
+      });
+
+      callback(null, polygonsCopy);
+    }, function(polygons, callback) {
+      // Triangulate
+      var allFaces = [];
+
+      // TODO: Support polygons with holes
+      _.each(polygons, function(polygon) {
+        // Triangulate faces
+        var faces = triangulate(polygon);
+
+        allFaces.push(faces);
+      });
+
+      callback(null, polygons, allFaces);
+    }, function(polygons, faces, callback) {
+      // Create OBJ using polygons and faces
+      var objStr = polygons2obj(polygons, faces, true);
+
+      callback(null, objStr);
+    }, function(objStr, callback) {
+      // Save OBJ file
+      var outputPath = path.join(objPath, id + ".obj");
+
+      // TODO: Fix huge delay before these small files appear in the filesystem when using fsextra.outputFile
+      // - Possibly a local issue with my filesystem rather than a Node or app issue
+      // - Perhaps batching will help, as well as deferring saving until CityGML buffer processing is complete
+      fs.outputFile(outputPath, objStr, function(err) {
+        if (err) {
+          console.error(err);
+        }
+      });
+    }]);
   });
 
   saxStream.on("end", function() {});
-
-  var start = microtime.now();
 
   var readStream = fs.createReadStream(citygmlPath);
   readStream.pipe(saxParser);
